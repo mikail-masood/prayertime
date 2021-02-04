e62c4897ee94cc516e3c9483456a7e91e8e3d4a0
Mikail <mikail.masood@mail.utoronto.ca>
2021-02-04 18:58:16 -0500

    added connection to new view, to be removed when but in the main page

--- lib/widgets/viewToggle.dart
+++ lib/widgets/viewToggle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/dailyView.dart';
 
 class ViewToggle extends StatelessWidget {
   @override
@@ -10,28 +11,34 @@ class ViewToggle extends StatelessWidget {
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(8.0),
-              height: 75.0,
-              width: 150.0,
-              decoration: BoxDecoration(
-                color: Color(0xFF527D6E),
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color(0xFF527D6E).withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 4,
-                    offset: Offset(0, 3),
-                  )
-                ],
-              ),
-              child: Text(
-                'Weekly',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DailyView()));
+              },
+              child: Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(8.0),
+                height: 75.0,
+                width: 150.0,
+                decoration: BoxDecoration(
+                  color: Color(0xFF527D6E),
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFF527D6E).withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: Offset(0, 3),
+                    )
+                  ],
+                ),
+                child: Text(
+                  'Weekly',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
                 ),
               ),
             ),
