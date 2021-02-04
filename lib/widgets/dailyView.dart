507d31eb3196eeb598adf5545528f92cf9d287cf
Mikail <mikail.masood@mail.utoronto.ca>
2021-02-04 18:57:45 -0500

    new view to be put on the main page

--- lib/widgets/dailyView.dart
+++ lib/widgets/dailyView.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import '../data.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class DailyView extends StatefulWidget {
   @override
@@ -6,8 +10,157 @@ class DailyView extends StatefulWidget {
 }
 
 class _DailyViewState extends State<DailyView> {
+  Data list;
+
+  static String city = 'Toronto';
+  static String country = 'Canada';
+  static int method = 1;
+
+  final String url =
+      'http://api.aladhan.com/v1/timingsByCity?city=$city&country=$country&method=$method';
+
+  Future getPrayerTimeData() async {
+    http.Response res = await http.get(Uri.encodeFull(url), headers: {
+      "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    });
+    final data = jsonDecode(res.body);
+
+    list = Data.fromJson(data);
+
+    return list;
+  }
+
+  bool valueFajr = false;
+  bool valueDhuhr = false;
+  bool valueAsr = false;
+  bool valueMaghrib = false;
+  bool valueIsha = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF527D6E),
+        elevation: 0.0,
+      ),
+      body: FutureBuilder(
+        future: getPrayerTimeData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return Container(
+              padding: const EdgeInsets.all(22.0),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 100.0,
+                  ),
+                  Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Color(0xFF527D6E),
+                      ),
+                      padding: EdgeInsets.all(22.0),
+                      child: Column(
+                        children: <Widget>[
+                          CheckboxListTile(
+                            checkColor: Color(0xFF527D6E),
+                            activeColor: Colors.white,
+                            secondary: Icon(
+                              Icons.alarm_on,
+                              color: Colors.white,
+                            ),
+                            title: Text(
+                              'Fajr at ${snapshot.data.data.timings.fajr}',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            value: this.valueFajr,
+                            onChanged: (bool value) {
+                              setState(() {
+                                this.valueFajr = value;
+                              });
+                            },
+                          ),
+                          CheckboxListTile(
+                            checkColor: Color(0xFF527D6E),
+                            activeColor: Colors.white,
+                            secondary: Icon(
+                              Icons.alarm_on,
+                              color: Colors.white,
+                            ),
+                            title: Text(
+                              'Dhuhr at ${snapshot.data.data.timings.dhuhr}',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            value: this.valueDhuhr,
+                            onChanged: (bool value) {
+                              setState(() {
+                                this.valueDhuhr = value;
+                              });
+                            },
+                          ),
+                          CheckboxListTile(
+                            checkColor: Color(0xFF527D6E),
+                            activeColor: Colors.white,
+                            secondary:
+                                Icon(Icons.alarm_on, color: Colors.white),
+                            title: Text(
+                              'Asr at ${snapshot.data.data.timings.asr}',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            value: this.valueAsr,
+                            onChanged: (bool value) {
+                              setState(() {
+                                this.valueAsr = value;
+                              });
+                            },
+                          ),
+                          CheckboxListTile(
+                            checkColor: Color(0xFF527D6E),
+                            activeColor: Colors.white,
+                            secondary:
+                                Icon(Icons.alarm_on, color: Colors.white),
+                            title: Text(
+                              'Maghrib at ${snapshot.data.data.timings.maghrib}',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            value: this.valueMaghrib,
+                            onChanged: (bool value) {
+                              setState(() {
+                                this.valueMaghrib = value;
+                              });
+                            },
+                          ),
+                          CheckboxListTile(
+                            checkColor: Color(0xFF527D6E),
+                            activeColor: Colors.white,
+                            secondary: Icon(
+                              Icons.alarm_on,
+                              color: Colors.white,
+                            ),
+                            title: Text(
+                              'Isha at ${snapshot.data.data.timings.isha}',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            value: this.valueIsha,
+                            onChanged: (bool value) {
+                              setState(() {
+                                this.valueIsha = value;
+                              });
+                            },
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
