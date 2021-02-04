84f0741408745a638a10d4c84a6e36a1f63a5315
Mikail <mikail.masood@mail.utoronto.ca>
2021-02-04 17:17:41 -0500

    started a redo of the basic daily view

---
+++ lib/widgets/dailyView.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DailyView extends StatefulWidget {
+  @override
+  _DailyViewState createState() => _DailyViewState();
+}
+
+class _DailyViewState extends State<DailyView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
