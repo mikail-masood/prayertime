38ddd48634c564ef22dcd53294e521408d3a557c
Mikail <mikail.masood@mail.utoronto.ca>
2021-02-05 17:48:11 -0500

    changed connection to new daily view instea of homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './screens/homePage.dart';
+import './widgets/dailyView.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: DailyView(),
     );
   }
 }
