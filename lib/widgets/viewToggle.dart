2d06d77295478d9bdef281773edc366e805215f8
Mikail <mikail.masood@mail.utoronto.ca>
2021-02-08 19:01:24 -0500

    changed colours and shape of buttons, still no functionality

--- lib/widgets/viewToggle.dart
+++ lib/widgets/viewToggle.dart
@@ -7,69 +7,49 @@ class ViewToggle extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(8.0),
-              height: 75.0,
-              width: 150.0,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [Color(0xFF232526), Color(0xFF414345)]),
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 4,
-                    offset: Offset(0, 3),
-                  )
-                ],
-              ),
-              child: Text(
-                'Weekly',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(8.0),
+                height: 75.0,
+                width: 75.0,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF6F6F6),
+                  borderRadius: BorderRadius.circular(100.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: Offset(0, 3),
+                    )
+                  ],
                 ),
-              ),
-            ),
+                child: Icon(Icons.view_week)),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(8.0),
-              height: 75.0,
-              width: 150.0,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [Color(0xFF232526), Color(0xFF414345)]),
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 4,
-                    offset: Offset(0, 3),
-                  )
-                ],
-              ),
-              child: Text(
-                'Monthly',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(8.0),
+                height: 75.0,
+                width: 75.0,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF6F6F6),
+                  borderRadius: BorderRadius.circular(100.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: Offset(0, 3),
+                    )
+                  ],
                 ),
-              ),
-            ),
+                child: Icon(Icons.calendar_today)),
           ),
         ],
       ),
