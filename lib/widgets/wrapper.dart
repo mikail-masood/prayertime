f7bc2524b28917c31f73364e0800ea6fbba6d12d
Mikail <mikail.masood@mail.utoronto.ca>
2021-02-10 18:48:58 -0500

    checks if location has been previously selected or not

---
+++ lib/widgets/wrapper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../widgets/locationSelector.dart';
+import '../widgets/dailyView.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if (getLocationData().toString == null) {
+      return LocationSelector();
+    } else {
+      return DailyView();
+    }
+  }
+
+  getLocationData() async {
+    SharedPreferences locationCity = await SharedPreferences.getInstance();
+    SharedPreferences locationCountry = await SharedPreferences.getInstance();
+    String currentCity = locationCity.getString("currentCity");
+    String currentCountry = locationCountry.getString("currentCountry");
+    return [currentCity, currentCountry];
+  }
+}
