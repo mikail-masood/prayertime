98271d927d0a745b630b0de6899a96365027a3ca
Mikail <mikail.masood@mail.utoronto.ca>
2021-02-10 18:50:27 -0500

    changed main  to redirect to wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './widgets/dailyView.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:async';
+import './widgets/wrapper.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DailyView(),
+      home: Wrapper(),
     );
   }
 }
